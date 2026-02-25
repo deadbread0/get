e979f24ba1a866cb6c4505c5aa9e1cb0e7a7bc00
Vostrikova Kira <kirakaramisheva2007@gmail.com>
2026-02-25 19:03:10 +0300

    итог тут

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,24 @@
+import mcp4725_driver as mcp
+import signal_generator as sg
+
+amplitude = 3.2
+signal_frequency = 2
+sampling_frequency = 1000
+
+if __name__ == "__main__":
+    try:
+        amplitude = float(input("Введите амплитуду: "))
+        signal_frequency = float(input("Введите частоту сигнала: "))
+        sampling_frequency = float(input("Введите частоту дискретизации: "))
+        dac = mcp.MCP4725(5.0, verbose = False)
+        k = 0
+        while True:
+            voltage = amplitude*sg.get_sin_wave_amplitude(signal_frequency, 1 / sampling_frequency * k)
+            k += 1
+            sg.wait_for_sampling_period(sampling_frequency)
+            dac.set_voltage(voltage)
+    except ValueError:
+        print("это не число")
+
+    finally:
+        dac.deinit()
---
+++ get-dac/t_signal_generator.py
@@ -0,0 +1,13 @@
+import numpy as np
+import time
+
+def get_t_wave_amplitude(freq, time):
+    period = 0.5 / freq
+    offset = time % period
+    if (offset < period/2): 
+        return 2*offset/period
+    else: 
+        return 2-2*offset/period
+
+def wait_for_sampling_period(sampling_frequency):
+    time.sleep(1.0/sampling_frequency)
