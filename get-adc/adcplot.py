304e512e54ec682410b5c1cb625baac9ffcc8129
Vostrikova Kira <kirakaramisheva2007@gmail.com>
2026-03-04 19:33:31 +0300

    что то готовое

---
+++ get-adc/adcplot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10,6))
+    plt.plot(time, voltage)
+    # plt.plot(x, y)
+    plt.grid(visible = True)
+    # plt.xlim(3)
+    # plt.ylim(max_voltage)
+    plt.xlabel('t')
+    plt.ylabel('U')
+    plt.title('U(t)')
+    plt.show()
+def plot_sampling_period_hist(time):
+    sampling_periods = []
+    last = 0
+    for i in time:
+        sampling_periods.append(i - last)
+        last = i
+
+    plt.figure(figsize=(10,6))
+    plt.hist(sampling_periods)
+    plt.xlabel('t')
+    plt.ylabel('U')
+    plt.title('U(t)')
+    plt.xlim(0, 0.06)
+    plt.grid(visible = True)
+    plt.show()
+
+    
